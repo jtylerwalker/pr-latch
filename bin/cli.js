0233cfc62a5983978eebaac4798596d0aff55f12
Tyler Walker <jtwalker@ebsco.com>
2019-10-20 17:54:46 -0400

    removes unneeded files

--- bin/cli.js
+++ bin/cli.js
@@ -1,89 +1,119 @@
-const { fetchPulls } = require("../lib/git-actions.js.js");
-const { prGitFlow } = require("../lib/executor.js.js");
-const env = require("../pr-latch.env.json");
-const inquirer = require("inquirer");
-const LatchEnv = require("../lib/init-env.js.js");
-const Spawner = require("../lib/spawner.js.js");
+#!/usr/bin/env node
+const Prompts = require("../lib/prompts/prompts");
+const program = require("commander");
+const { fetchPulls } = require("../lib/git-actions");
+const { getEnvPids, prGitFlow, stopActivePorts } = require("../lib/executor");
+const env = require("../.latchrc.json");
+const Spawner = require("../lib/spawner");
+
 require("dotenv").config();
 
-const init = new LatchEnv();
-const { HOME } = process.env;
-const uiDir = `${env.projects.ui.projectDirectory}`;
-const edgeDir = `${env.projects.edge.projectDirectory}`;
+program.version("0.0.1");
 
-const autoReview = () => {
-  return new Promise(async res => {
-    const { pull } = await fetchPulls("discover.shared.ebsconext-ui").catch(
+/**
+ * Commander: List env projects
+ */
+const listEnvs = () => Prompts.static.list(env.projects);
+
+/**
+ * @param {String} alias
+ * Commander: PR review flow for stipulated env
+ */
+const parseAlias = alias => {
+  return new Promise(async (res, _) => {
+    const { pull } = await fetchPulls(env.projects[`${alias}`].name).catch(
       err => console.warn(err)
     );
 
-    await prGitFlow(pull.branch, uiDir);
+    await prGitFlow(pull.branch, env.projects[`${alias}`].directory);
     res();
   });
 };
 
 const pollForServerUp = spawn => {
-  return new Promise(async (resolve, reject) => {
+  return new Promise(async (res, _) => {
     let isUp = false;
 
     while (!isUp) {
       isUp = await spawn.pingServer();
-      isUp === true && resolve(spawn.serverUp(isUp));
+      isUp === true && res(spawn.serverUp(isUp));
     }
   });
 };
 
-const generateEnvs = () => {
-  const uiFork = new Spawner("Ui done loading", "3030");
-  const edgeFork = new Spawner("Edge done loading", "8080");
-  const authFork = new Spawner("Goggles done loading", "4040");
+/**
+ * @param {String} alias
+ * Commander: PR review flow for stipulated env
+ */
+const startEnvironment = alias => {
+  const { port, directory, startCommand } = env.projects[`${alias}`];
+  const projectFork = new Spawner(`${alias.toUpperCase()} up and ready`, port);
 
-  return new Promise(async (resolve, reject) => {
-    edgeFork.spawnAndSpin("npm", ["run", "dev"], edgeDir);
-    await pollForServerUp(edgeFork);
-    uiFork.spawnAndSpin("npm", ["run", "dev"], uiDir);
-    await pollForServerUp(uiFork);
-    authFork.spawnAndSpin(
-      "npx",
-      ["@ebsco/auth-goggles", "--config", `${HOME} /.auth - goggles.yaml`],
-      `${HOME}`
+  return new Promise(async (res, _) => {
+    projectFork.spawnAndSpin(
+      `${startCommand[0]}`,
+      startCommand.slice(1),
+      directory
     );
-    await pollForServerUp(authFork);
-    resolve();
+
+    await pollForServerUp(projectFork);
+
+    res({ hasLoaded: true });
   });
 };
 
-const initialize = async () => {
-  console.clear();
-  showTitleBar();
-  await init.initializeProject();
-  console.clear();
-  showSegueBar();
-  await autoReview();
-  console.clear();
-  showSegueBar();
+/**
+ * Commander: Generate new env project
+ */
+const envNew = () => {
+  const {
+    promptForMainProjectDirectory,
+    promptForProjectSettings
+  } = require("../lib/latch-env");
+
+  env.mainDirectory
+    ? promptForProjectSettings(env.mainDirectory)
+    : promptForMainProjectDirectory();
 };
 
-const startLocalEnv = async () => {
-  await generateEnvs();
-  console.clear();
-  showSegueBar();
+/**
+ * start envs
+ * @param {[String]} aliases
+ */
+const envsUp = aliases => {
+  const { aggregateEnvPids } = require("../lib/latch-env");
+
+  Promise.all(aliases.map(async a => await startEnvironment(a))).then(() =>
+    process.exit(1)
+  );
+
+  aggregateEnvPids(aliases.map(a => env.projects[`${a}`].port));
 };
 
-setTimeoutCurl = count => {
-  console.log(count);
-  count >= 0 &&
-    setTimeout(function() {
-      setTimeoutCurl(count - 1);
-      return console.log("hello");
-    }, 2000);
+/**
+ * stop processes associated with running envs
+ */
+const envsDown = async () => {
+  const { clearEnvPids } = require("../lib/latch-env");
+
+  return Promise.all(
+    env.activePorts.map(async p => await getEnvPids(p, env))
+  ).then(std => {
+    const pids = std.reduce(
+      (acc, { stdout }) => acc.concat(stdout.split("\n")).filter(Boolean),
+      []
+    );
+
+    stopActivePorts(pids, env.mainDirectory);
+
+    clearEnvPids();
+  });
 };
 
-const program = require("commander");
-program.version("0.0.1");
+program.command("env-up [aliases...]").action(envsUp);
+program.command("env-down").action(envsDown);
+program.command("env-new").action(envNew);
+program.command("list").action(listEnvs);
+program.command("review <alias>").action(parseAlias);
 
 program.parse(process.argv);
-const [, , ...args] = process.argv;
-
-if (args[0] == "new" && args[1] == "env") init.promptForProjectSettings();
-if (program.debug) console.log(program.opts());
